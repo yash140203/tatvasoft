e5848dd7148a2edcecf4fb44d8456f9c32f2b5d0
Yash Patel <yashpatel0979@gmail.com>
2024-06-09 12:15:40 +0530

    first commit

---
+++ CIPIWebApi-main/CIPlatfromWebAPI/Controllers/MissionController.cs
@@ -0,0 +1,205 @@
+﻿using Business_logic_Layer;
+using Data_Access_Layer.Repository.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Reflection;
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionController : ControllerBase
+    {
+        private readonly BALMission _balMission;
+        private readonly ResponseResult result = new ResponseResult();
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+
+        public MissionController(BALMission balMission, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            _balMission = balMission;
+            _environment = environment;
+        }
+
+        [HttpGet]
+        [Route("MissionList")]
+        public ResponseResult MissionList()
+        {
+            try
+            {
+                result.Data = _balMission.MissionList();
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        [HttpPost]
+        [Route("AddMission")]
+        public ResponseResult AddMission(Missions mission)
+        {
+            try
+            {
+                result.Data = _balMission.AddMission(mission);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+        [HttpPost]
+        [Route("UploadImage")]
+        public async Task<IActionResult> UploadImage([FromForm] List<IFormFile> upload)
+        {
+            try
+            {
+                string filePath = "";
+                string fullPath = "";
+                var files = Request.Form.Files;
+                List<string> fileList = new List<string>();
+                if (files != null && files.Count > 0)
+                {
+                    foreach (var file in files)
+                    {
+                        string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        filePath = Path.Combine("UploadMissionImage", "Mission");
+                        string fileRootPath = Path.Combine(_environment.WebRootPath, filePath);
+
+                        if (!Directory.Exists(fileRootPath))
+                        {
+                            Directory.CreateDirectory(fileRootPath);
+                        }
+
+                        string name = Path.GetFileNameWithoutExtension(fileName);
+                        string extension = Path.GetExtension(fileName);
+                        string fullFileName = name + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
+                        fullPath = Path.Combine(fileRootPath, fullFileName);
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                        fileList.Add(fullPath);
+                    }
+                }
+                return Ok(fileList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("MissionDetailById/{id}")]
+        public async Task<ResponseResult> MissionDetailById(int id)
+        {
+            try
+            {
+                result.Data = await _balMission.MissionDetailById(id);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        [HttpPut("UpdateMission")]
+        public async Task<ResponseResult> UpdateMission(Missions mission)
+        {
+            try
+            {
+                result.Data = await _balMission.UpdateMission(mission);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+        [HttpDelete("DeleteMission/{id}")]
+        public async Task<ResponseResult> DeleteMission(int id)
+        {
+            try
+            {
+                result.Data = await _balMission.DeleteMission(id);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+        [HttpGet("GetMissionThemeList")]
+        public async Task<ResponseResult> GetMissionThemeList()
+        {
+            try
+            {
+                result.Data = await _balMission.GetMissionThemeList();
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+        [HttpGet("GetMissionSkillList")]
+        public async Task<ResponseResult> GetMissionSkillList()
+        {
+            try
+            {
+                result.Data = await _balMission.GetMissionSkillList();
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+        [HttpGet]
+        [Route("MissionApplicationApprove/{id}")]
+        public ResponseResult MissionApplicationApprove(int id)
+        {
+            try
+            {
+                result.Data = _balMission.MissionApplicationApprove(id);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+        [HttpGet("MissionApplicationDelete/{id}")]
+        public ResponseResult MissionApplicationDelete(int id)
+        {
+            try
+            {
+                result.Data = _balMission.MissionApplicationDelete(id);
+                result.Result = ResponseStatus.Success;
+            }catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+    }
+}
